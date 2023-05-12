7324124ac691a49d6290b37515f9bb25782120f0
Minnullin <minnullinilshat@gmail.com>
2023-05-12 16:40:22 +0300

    add db, tables and signup

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-engine = create_engine('postgresql://pzdelivery:pzdelivery@localhost/pizza_delivery',
+engine = create_engine('postgresql+psycopg2://smilecook:smilecook@localhost/pizza_delivery',
                        echo=True)
 
 Base = declarative_base()
+
+Session = sessionmaker()
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from auth_routes import auth_router 
 from order_routes import order_router
+import uvicorn
+
 
 app = FastAPI() 
 
 
 app.include_router(auth_router)
 app.include_router(order_router)
+
+
+if __name__ == '__main__': 
+    uvicorn.run("main:app", host='127.0.0.1', port=5000, reload=True)
